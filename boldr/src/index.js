TJ178/BOULDR

commit 4688de744ebf4b56cf77cdc89387d33c8a3488d1

setting up firebase

boldr/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouterm, Switch, Route, Link } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 

boldr/src/firebase-config.js
@@ -1,0 +1,14 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD-LrtA3qTeynuuSvyYgcIrcY35lSId0Wg",
+    authDomain: "boldr-f2e1c.firebaseapp.com",
+    projectId: "boldr-f2e1c",
+    storageBucket: "boldr-f2e1c.appspot.com",
+    messagingSenderId: "532041021603",
+    appId: "1:532041021603:web:703553aa898eb522cc2716"
+};
+  
+const app = firebase.initializeApp(firebaseConfig);
+
+export default app;

commit 2a73607bbef7eda1452c5fd273d2d59398abbb99

Fix typo introduced in 4688de7

boldr/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouterm, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 