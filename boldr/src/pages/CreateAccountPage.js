153622157276a1e39bb8befa039a104800382f32
Aaron Lu <aaronlu521@gmail.com>
2022-02-23 14:40:36 +0900

    loginpage + createpage set up

--- boldr/src/pages/CreateAccountPage.js
+++ boldr/src/pages/CreateAccountPage.js
@@ -1,7 +1,138 @@
 import React from "react";
+import ReactDOM from 'react-dom';
+import './CreateAccountPage.css';
+import { Link } from 'react-router-dom';
 
-function CreateAccountPage(props) {
-    return <div>Hello from create account page</div>
+class CreateAccountPage extends React.Component
+{
+	constructor(props) {
+		super(props)
+		this.state = {
+			username: '',
+			user: '',
+			password: '',
+			password2: '',
+			warning: true,
+			success: false,
+		};
+		this.handleChange = this.handleChange.bind(this);
+		this.handleChangePass = this.handleChangePass.bind(this);
+		this.handleChangePass2 = this.handleChangePass2.bind(this);
+	}
+
+	handleChange(e) {
+		this.setState({username: e.target.value})
+	}
+
+	handleChangePass(e) {
+		this.setState({password: e.target.value})
+	}
+
+	handleChangePass2(e) {
+		this.setState({password2: e.target.value})
+	}
+
+	addNewUser() {
+		const data = {
+			username: this.state.username,
+			password: this.state.password,
+			posts: [],
+		}
+		fetch("http://localhost:3000/profiles", {
+			method: 'POST',
+			mode: 'cors',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(data)
+		})
+			.then(res => res.json())
+			.then(res => console.log(res))
+	}
+	
+	retrieveUser() {
+        const username = this.state.username;
+        //console.log(username);
+        fetch(`http://localhost:3000/profiles/${username}`)
+            .then(res => res.json()
+            .then(res => 
+			this.setState({
+				user: res.username, 
+			}
+			))
+    )}
+	
+	checkPassword() {
+		    this.retrieveUser();
+            if (this.state.password != '' && this.state.password != ''){
+            	this.state.warning = false;
+				if (this.state.password == this.state.password2) {
+					if(this.state.username != this.state.user)
+					{
+						console.log(this.state.username)
+						console.log("")
+						console.log(this.state.user)
+						this.addNewUser();
+						this.state.username = '';
+						this.state.success = true;
+					}
+					else
+					{
+						this.state.success = false;
+					}
+				} 
+				else 
+				{
+					this.state.success = false;
+				}
+				//console.log(this.state.password, this.state.password2);
+				this.state.password = '';
+				this.state.password2 = '';
+				
+            } 
+	}
+
+	render()
+	{
+		const vis_style = this.state.warning ? 'hidden' : 'visible';
+		const message = this.state.success ? 'Account created! Go to the sign in page to login.' : 'Passwords do not match or username is taken. Please try again!'
+		return (
+			<body class = "signup-container"> 
+				
+				<div class = "signup-window"> 
+					<h1 class = "signup-header"> New Account </h1>	
+						
+					<form>
+						<h2 class = "signup-label"> USERNAME </h2>
+						<div class = "usernames">
+						<input type="form_control" value={this.state.username} onChange={this.handleChange}/>
+						</div>
+					</form>
+			
+					<form>
+						<h3 class ="signup-label"> PASSWORD </h3>
+						<div class = "usernames">
+						<input type="password" value={this.state.password} onChange={this.handleChangePass}/>
+						</div>
+					</form>
+				
+					<form>
+						<h3 class ="signup-label"> VERIFY PASSWORD </h3>
+						<div class = "verifypass">
+						<input id='ending' type="password" value={this.state.password2} onChange={this.handleChangePass2}/>
+						</div>
+					</form>
+					
+					<Link type="submit_i" onClick={() => this.checkPassword()} style={{color: '#282b30'}}> Submit </Link>
+					<Link type="submit_j" to = {"/signin"} style={{color: '#282b30'}}> Go back </Link>
+                    
+
+				</div>
+				<h5 id="warning" style={{visibility: vis_style}}> {message} </h5>
+				
+			</body>
+		);
+	}
 }
 
 export default CreateAccountPage;
--- boldr/src/pages/LoginPage.js
+++ boldr/src/pages/LoginPage.js
@@ -1,112 +1,160 @@
 import React from "react";
-import { db, app, storage } from "../firebase-config.js";
-import LoginError from './alert.js';
+import ReactDOM from 'react-dom';
+import { Link } from 'react-router-dom';
+import './LoginPage.css';
 
-class LoginPage extends React.Component {
+class LoginPage extends React.Component
+{
+	constructor(props) {
+		super(props);
+		this.state = {
+			username: '',
+			password: '',
+			user: '',
+			pw: '',
+			click: false,
+			tempu : '',
+			tempp :'',
+			warning: false,
+			case1: false,
+			clicked: false
+		}
+		this.handleChange = this.handleChange.bind(this);
+		this.handleChangeP = this.handleChangeP.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
 
-  constructor(props) {
-    super(props);
-    this.state = {
-        error : false,
-        modal: false,
-    };
+	handleChange(e) {
+		this.state.clicked = false;
+		this.setState({username: e.target.value})
+	}
+	
+	handleChangeP(e) {
+		this.state.clicked = false;
+		this.setState({password: e.target.value})
+	}
 
-    this.signUp = this.signUp.bind(this);
-    this.login = this.login.bind(this);
-    this.setError=this.setError.bind(this);
-  }
+	saveSelf() {
+		const data = {
+			username: this.state.username,
+			password: this.state.password,
+		}
 
-  componentDidUpdate()
-  {
-    if(this.state.error === true)
-    {
-      const timer = setTimeout(() => {
-        this.setError();
-      }, 6000);
+		// this.state.username = username that the user input
+		// retrieve the user object corresponding the this username from the database
+			// fetch(
 
-      return () => {
-      clearTimeout(timer);
-      };
+			//) -> return the user object corresponding to the username
+		// compare user object password with this.state.password
+		// 
+
+		// only call this if passwords match
+		fetch("http://localhost:3001/self", {
+			method: 'POST',
+			mode: 'cors',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(data)
+		})
+			.then(res => res.json())
+			.then(res => console.log(res))
+	}
+	
+	retrieveUser() {
+        const username = this.state.username;
+        //console.log(username);
+        fetch(`http://localhost:3000/profiles/${username}`)
+			//.then(response => console.log(response.json()))
+            .then(response => response.json())
+            .then(response => 
+			this.setState({ 
+				user: response.username, 
+				pw: response.password
+			}, () => { 
+				//console.log(this.state.user)
+				//console.log(this.state.pw)
+			}));
     }
-    else
-    {
-      return null;
-    }
-  }
+	
+	checkOnClick()
+	{
+		if ((this.state.user == this.state.username) && this.state.user != "") 
+		{
+			if (this.state.pw == this.state.password)
+			{
+                this.warning = false;
+				this.handleSubmit()
+				return "/main";
+				//console.log(4)
+			} else {
+				this.state.warning = true;
+				this.state.case1 = true;
+				console.log(1)
+			}
+		} else if (this.state.username !== '' && this.state.password !== '') {
+			this.state.warning = true;
+			this.state.case1 = false;
+			console.log(2);
+		}
+		return "/";
+	}
+	
+	handleSubmit()
+	{
+		return this.saveSelf();
+	}
+	
+	click()
+	{
+		this.retrieveUser();
+		this.state.clicked = true;
+		//console.log(3);
+	}
 
-  setError() {
-    this.setState({
-      error:false
-    });
-  }
+	reset_click() {
+		this.state.clicked = false;
+		this.state.warning = false;
+	}
 
-  signUp() {
+	render()
+	{
+		console.log(this.state.warning, "w");
+		console.log(this.state.clicked, "c");
+		const vis_style = (this.state.warning && this.state.clicked == true) ? 'visible' : 'hidden';
+		const message = this.state.case1 ? 'Invalid password. Please try again!' : 'This account does not exist. Please register a new account!';
+		var page = this.checkOnClick();
+		return (
+				<body>
+					<div class = "signin-shadow"> </div>
+					
+					<div class = "signin-window"> 
+						<h1 class = "signin-header"> User Login </h1>	
+					<h6> Any warning for login</h6>
+						<form>
+							<h2 class = "signin-label"> USERNAME </h2>
+							<div class = "username">
+							<input type="form_control" value={this.state.username} onChange={this.handleChange} onkeydown={() => this.reset_click}/>
+							</div>
+						</form>
 
-    const email = document.querySelector('#email').value;
-    const password = document.querySelector('#password').value;
+						<form>
+							<h3 class ="signin-label"> PASSWORD </h3>
+							<div class = "username">
+							<input type="password" value={this.state.password} onChange={this.handleChangeP} onkeydown={() => this.reset_click}/>
+							</div>
+						</form>
+						
+						<Link className="signin-newAccount" to={"/CreateAccountPage"} > Register an account </Link>
+                        
+						<Link type="submit_i" to = {page} onClick={() => this.click()} style={{color: '#282b30'}}> Submit </Link>
 
-    
-    app.auth().createUserWithEmailAndPassword(email, password)
-      .then((u) => {
-        console.log('Successfully Signed Up');
-        this.setState({
-          error:false
-        });
-      })
-      .catch((err) => {
-        console.log('Error: ' + err.toString());
-        this.setState({
-          error:true
-        });
-      })
-  }
-
-  login() {
-    const email = document.querySelector('#email').value;
-    const password = document.querySelector('#password').value;
-    app.auth().signInWithEmailAndPassword(email, password)
-      .then((u) => {
-        console.log('Successfully Logged In');
-        this.setState({
-          error:false
-        });
-      })
-      .catch((err) => {
-        console.log('Error: ' + err.toString());      
-        this.setState({
-          error:true
-        });
-      })
-  }
-
-  render() {
-    return (
-      <>
-        { this.state.error && ( 
-        <LoginError/>)
-        }
-
-      <div className = "login">Login</div>
-
-      <div className = "container">
-        <div>
-          <div>Email</div>
-          <input id="email" placeholder="Enter Email.." type="text"/>
-        </div>
-        <div>
-          <div>Password</div>
-          <input id="password" placeholder="Enter Password.." type="text"/>
-        </div>
-        <button style={{margin: '10px'}} onClick={this.login}>Login</button>
-        <button style={{margin: '10px'}} onClick={this.signUp}>Sign Up</button>
-        
-      
-  
-      </div>
-     </>
-    );
-
-  }
+					</div>
+					
+					<h5 id="warning" style={{visibility: vis_style}}> {message} </h5>
+				</body>
+		);
+	}
 }
 
 export default LoginPage;
