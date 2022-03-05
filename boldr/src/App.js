5551d8f53625e15a40fe212a6c085407cee65337
Maya Chatrathi <maya@chatrathi.com>
2022-03-05 15:53:03 -0800

    Merging main

--- boldr/src/App.js
+++ boldr/src/App.js
@@ -1,35 +1,47 @@
-import React from 'react';
 import { Route, Routes } from "react-router-dom";
 
 import HomePage from "./pages/HomePage";
-import LoginPage from "./pages/LoginPage.js";
-import CreateAccountPage from "./pages/CreateAccountPage.js";
+import LoginPage from "./pages/LoginPage";
+import CreateAccountPage from "./pages/CreateAccountPage";
 import ProfilePage from "./pages/ProfilePage";
+import gymPic from "./assets/gymPic.png";
 import Layout from "./components/layout/Layout";
 import ProblemDetailsPage from "./pages/ProblemDetailsPage";
-import AddProblemPage from "./pages/AddProblemPage";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { AuthProvider } from './contexts/AuthContext';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <AuthProvider>
-     <Layout>
+    <Layout>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/problem-details" element={<ProblemDetailsPage/>}>
-          <Route path=":problemId" element={<ProblemDetailsPage/>} />
-        </Route>
+        <Route path="/problem-details" element={<ProblemDetailsPage prob={tempProbs[0]} />} />
         <Route path="/create-account" element={<CreateAccountPage />} />
         <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/add-problem" element={<AddProblemPage />} />
       </Routes>
     </Layout>
-    </AuthProvider>
-    
   );
 }
 
 export default App;
+
+const tempProbs = [
+  {
+    id: 1,
+    image: gymPic,
+    title: "A Silly Little Problem",
+    isFavorite: false,
+    gym: "Wooden",
+    description: "Go touch rock",
+    rating:3.5,
+    difficulty: 1,
+  },
+  {
+    id: 2,
+    image: gymPic,
+    title: "Something is going up?",
+    isFavorite: false,
+    gym: "Cliffs of Id",
+    description: "Go touch rock but at this gym",
+  },
+];
