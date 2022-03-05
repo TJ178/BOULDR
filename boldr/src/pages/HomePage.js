7a050383021db1adfd47fd36ff600f46d370723e
TJ178 <tjacques888@gmail.com>
2022-03-04 22:34:37 -0800

    Add searchbar html, searchbar routing

--- boldr/src/pages/HomePage.js
+++ boldr/src/pages/HomePage.js
@@ -4,13 +4,23 @@ import { db } from "../firebase-config.js";
 import { collection } from 'firebase/firestore';
 import { convertCollectionToProblems } from '../FirebaseSupport.js';
 import { useCollectionOnce } from 'react-firebase-hooks/firestore'
+import { Form } from "react-bootstrap";
+import { useNavigate } from 'react-router-dom';
 
 function HomePage() {
   const [data, loading, error] = useCollectionOnce(collection(db, 'problems'));
+  let navigate = useNavigate();
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    navigate("/?=" + event.target.search.value);
+  }
 
   return (
     <section>
-      <h1 style={{textAlign: "center"}}> Add the searchbar here </h1>
+      <Form onSubmit={handleSubmit}>
+        <Form.Control type='text' placeholder='Search...' size='lg' name='search'/>
+      </Form>
       <h1>Recent Activity</h1>
       {error && <p><strong>Error Loading Problems: {JSON.stringify(error)}</strong></p>}
       {loading && <p><span>Loading...</span></p>}
