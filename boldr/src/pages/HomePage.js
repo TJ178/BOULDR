3753b9834fea9c6b41fa5ec57ea98315c539b792
Preston Rooker <prestonrooker@gmail.com>
2022-03-05 13:13:41 -0800

    Removed 'Recent Activity' header in HomePage.

--- boldr/src/pages/HomePage.js
+++ boldr/src/pages/HomePage.js
@@ -30,7 +30,6 @@ function HomePage() {
         <Form.Control type='text' placeholder={'Search...'} size='lg' name='search' 
                       defaultValue={searchParams ? searchParams.get('keyword') : ""} onChange={handleChange}/>
       </Form>
-      <h1>Recent Activity</h1>
       {error && <p><strong>Error Loading Problems: {JSON.stringify(error)}</strong></p>}
       {loading && <p><span>Loading...</span></p>}
       {data && !searchParams && <ProblemList problems={convertCollectionToProblems(data)} />}
