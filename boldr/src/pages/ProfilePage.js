12dd5b50d96c1c0fad1c40c9631b41c6ad187810
TJ178 <tjacques888@gmail.com>
2022-03-05 17:04:54 -0800

    Add profile picture to Profile Page
    	modified:   src/pages/ProfilePage.js
        src/pages/ProfilePage.module.css
    
    	modified:   src/pages/ProfilePage.js
    	new file:   src/pages/ProfilePage.module.css

--- boldr/src/pages/ProfilePage.js
+++ boldr/src/pages/ProfilePage.js
@@ -2,11 +2,27 @@ import React, { useState } from "react"
 import { Card, Button, Alert } from "react-bootstrap"
 import { useAuth } from "../contexts/AuthContext"
 import { useNavigate } from "react-router-dom"
+import { useDownloadURL } from 'react-firebase-hooks/storage';
+import { ref } from 'firebase/storage';
+import { storage } from '../firebase-config.js'
+import classes from "./ProfilePage.module.css";
 
 export default function ProfilePage() {
   const [error, setError] = useState("")
   const { currentUser, logout } = useAuth()
   const navigate = useNavigate()
+
+  let photoRef;
+  if(currentUser){
+    if(currentUser.photoURL){
+      photoRef = ref(storage, currentUser.photoURL)
+    }else{
+      photoRef = ref(storage, "gs://boldr-f2e1c.appspot.com/media/usr.png")
+    }
+  }else{
+    photoRef = ref(storage, "gs://boldr-f2e1c.appspot.com/media/usr.png")
+  }
+  const [image, loading, imageError] = useDownloadURL(photoRef);
   
   async function handleLogout() {
     setError("")
@@ -25,6 +41,8 @@ export default function ProfilePage() {
         <Card.Body>
           <h2 className="text-center mb-4">Profile</h2>
           {error && <Alert variant="danger">{error}</Alert>}
+          <img className={classes.profile} src={image} />
+          <br/>
           <strong>Email:</strong> {currentUser.email}
           <br/>
           <strong>Name: </strong> {currentUser.displayName}
