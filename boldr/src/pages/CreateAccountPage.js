a9074e232d019eaf646f237bd73a0600766a9ff4
Aaron Lu <aaronlu521@gmail.com>
2022-03-07 15:42:39 +0900

    gym names added/corrected

--- boldr/src/pages/CreateAccountPage.js
+++ boldr/src/pages/CreateAccountPage.js
@@ -158,9 +158,9 @@ export default function CreateAccountPage() {
                 }}
               >
                 <option>Choose your home gym:</option>
-                <option value="Cliffs of Id">Gym 1</option>
-                <option value="Wooden">Gym 2</option>
-                <option value="Rockcreation">Gym 3</option>
+                <option value="Cliffs of Id">Cliffs of Id</option>
+                <option value="Rockcreation">Rockcreation</option>
+                <option value="Wooden">Wooden</option>
               </Form.Control>
             </Form.Group>
             <Form.Group id="staff">
