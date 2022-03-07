c78a0fb851477e0fa44084897fe8b9ed5107154b
Preston Rooker <prestonrooker@gmail.com>
2022-03-06 16:53:58 -0800

    Added ClimbingBackground component.
    Moved the divs for for climbing background image and gradient to
    be its own component so it can be used in both login and create
    account.

--- boldr/src/pages/LoginPage.js
+++ boldr/src/pages/LoginPage.js
@@ -1,6 +1,7 @@
 import React, { useRef, useState } from "react";
 import { Form, FloatingLabel, Button, Card, Alert } from "react-bootstrap";
 import { Link, useNavigate, Navigate } from "react-router-dom";
+import ClimbingBackground from "../components/background/ClimbingBackground";
 import classes from "./LoginPage.module.css";
 import background from "../assets/climbing_background.jpg";
 import logo from "../assets/BOULDR_Logo_Lg.png";
@@ -30,53 +31,48 @@ export default function CreateAccountPage() {
   return (
     <>
       {currentUser ? <Navigate to="/" /> : null}
-      <div
-        className={classes.background}
-        style={{ backgroundImage: `url(${background})` }}
-      >
-        <div className={classes.gradient}>
-          <div className={classes.logincontents}>
-            <img src={logo} alt="BOULDR" style={{paddingBottom: "10%", paddingRight: "2%"}}/>
-            <h4 style={{paddingBottom: "1.25%"}}>Ready to climb?</h4>
-			{currentUser && currentUser.email}
-			{error && <Alert variant="danger"> {error} </Alert>}
-            <div className={classes.flexbox}>
-              <Form onSubmit={handleSubmit} style={{ width: "90%" }}>
-                <FloatingLabel
-                  label="Email"
-                  id="email"
-                  className="mb-3"
-                >
-                  <Form.Control
-                    type="email"
-                    placeholder="Email Address"
-                    ref={emailRef}
-                    required
-                  />
-                </FloatingLabel>
-                <FloatingLabel label="Password" id="password" className="mb-3">
-                  <Form.Control
-                    type="password"
-                    placeholder="Password"
-                    ref={passwordRef}
-                    required
-                  />
-                </FloatingLabel>
-                <Button
-                  disabled={loading}
-                  type="submit"
-                  style={{ width: "100%" }}
-                >
-                  Log In
-                </Button>
-              </Form>
-            </div>
-            <div className="fs-5 mt-2">
-              Need an account? <Link to="/create-account">Sign Up</Link>
-            </div>
+      <ClimbingBackground>
+        <div className={classes.logincontents}>
+          <img
+            src={logo}
+            alt="BOULDR"
+            style={{ paddingBottom: "10%", paddingRight: "2%" }}
+          />
+          <h4 style={{ paddingBottom: "1.25%" }}>Ready to climb?</h4>
+          {currentUser && currentUser.email}
+          {error && <Alert variant="danger"> {error} </Alert>}
+          <div className={classes.flexbox}>
+            <Form onSubmit={handleSubmit} style={{ width: "90%" }}>
+              <FloatingLabel label="Email" id="email" className="mb-3">
+                <Form.Control
+                  type="email"
+                  placeholder="Email Address"
+                  ref={emailRef}
+                  required
+                />
+              </FloatingLabel>
+              <FloatingLabel label="Password" id="password" className="mb-3">
+                <Form.Control
+                  type="password"
+                  placeholder="Password"
+                  ref={passwordRef}
+                  required
+                />
+              </FloatingLabel>
+              <Button
+                disabled={loading}
+                type="submit"
+                style={{ width: "100%" }}
+              >
+                Log In
+              </Button>
+            </Form>
+          </div>
+          <div className="fs-5 mt-2">
+            Need an account? <Link to="/create-account">Sign Up</Link>
           </div>
         </div>
-      </div>
+      </ClimbingBackground>
     </>
   );
 }
